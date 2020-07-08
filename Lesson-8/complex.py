b87a5b1053efbb8977bce21d8252f333aacb0a73
StasenkoAV <jahdie@yandex.ru>
2020-07-09 04:04:26 +0700

    clear code for TASK#7

--- Lesson-8/complex.py
+++ Lesson-8/complex.py
@@ -1,31 +1,41 @@
 # Реализовать проект «Операции с комплексными числами». Создайте класс «Комплексное число», реализуйте перегрузку
 # методовсложения и умножения комплексных чисел. Проверьте работу проекта, создав экземпляры класса (комплексные числа)
 # и выполнив сложение и умножение созданных экземпляров. Проверьте корректность полученного результата.
-z1 = "-2+3i"
-z2 = "-1+i"
-z1_1 = z1.replace("i", "")
-z2_1 = z2.replace("i", "")
-new_list1 = ['-2', '+3']
-new_list2 = ['-1', '1']
-for i in range(1, len(z1_1)):
-    if z1_1[i] == "-" or z1_1[i] == "+":
-        new_list1.append(z1_1[:i])
-        new_list1.append(z1_1[i:])
-for i in range(1, len(z2_1)):
-    if z2_1[i] == "-" or z2_1[i] == "+":
-        new_list2.append(z2_1[:i])
-        new_list2.append(z2_1[i:])
-print(new_list1)
-print(new_list2)
-z = []
-z3 = []
-z.append(int(new_list1[0])+int(new_list2[0]))
-z.append(int(new_list1[1])+int(new_list2[1]))
-z3.append((int(new_list1[0])*int(new_list2[0]))-(int(new_list1[1])*int(new_list2[1])))
-z3.append((int(new_list1[1])*int(new_list2[0]))+(int(new_list1[0])*int(new_list2[1])))
+class MyComplex:
+    def __init__(self, complex_str):
+        self.complex_str = complex_str
 
-string = f"{z[0]}{'+' if z[1]>0 else ''}{z[1]}i"
-string1 = f"{z3[0]}{'+' if z3[1]>0 else ''}{z3[1]}i"
+    def __str__(self):
+        return f"{self.complex_str}"
 
-print(string)
-print(string1)
+    def __add__(self, other):
+        complex_add = []
+        complex_add.append(int(self.conversion(self.complex_str)[0]) + int(other.conversion(other.complex_str)[0]))
+        complex_add.append(int(self.conversion(self.complex_str)[1]) + int(other.conversion(other.complex_str)[1]))
+        return f"{self.complex_str}+{other.complex_str}={complex_add[0]}{'+' if complex_add[1] > 0 else ''}" \
+               f"{complex_add[1]}i"
+
+    def __mul__(self, other):
+        complex_mul = []
+        complex_mul.append((int(self.conversion(self.complex_str)[0]) * int(other.conversion(other.complex_str)[0])) -
+                           (int(self.conversion(self.complex_str)[1]) * int(other.conversion(other.complex_str)[1])))
+        complex_mul.append((int(self.conversion(self.complex_str)[1]) * int(other.conversion(other.complex_str)[0])) +
+                           (int(self.conversion(self.complex_str)[0]) * int(other.conversion(other.complex_str)[1])))
+        return f"{self.complex_str}+{other.complex_str}={complex_mul[0]}{'+' if complex_mul[1] > 0 else ''}" \
+               f"{complex_mul[1]}i"
+
+    @staticmethod
+    def conversion(complex_str):
+        complex_list = []
+        complex_str = complex_str.replace("i", "")
+        for index in range(1, len(complex_str)):
+            if complex_str[index] == "-" or complex_str[index] == "+":
+                complex_list.append(complex_str[:index])
+                complex_list.append(complex_str[index:])
+        return complex_list
+
+
+my_complex1 = MyComplex("-2+3i")
+my_complex2 = MyComplex("-1+3i")
+print(my_complex1 + my_complex2)
+print(my_complex1 * my_complex2)
