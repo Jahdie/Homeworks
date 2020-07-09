d604a953316228f72c2285d939850cdf509046e0
StasenkoAV <jahdie@yandex.ru>
2020-07-09 07:47:55 +0700

    almost done TASK#7

--- Lesson-8/complex.py
+++ Lesson-8/complex.py
@@ -12,8 +12,9 @@ class MyComplex:
         complex_add = []
         complex_add.append(int(self.conversion(self.complex_str)[0]) + int(other.conversion(other.complex_str)[0]))
         complex_add.append(int(self.conversion(self.complex_str)[1]) + int(other.conversion(other.complex_str)[1]))
-        return f"{self.complex_str}+{other.complex_str}={complex_add[0]}{'+' if complex_add[1] > 0 else ''}" \
-               f"{complex_add[1]}i"
+        return f"({self.complex_str})+({other.complex_str})={complex_add[0] if complex_add[0] !=0 else ''}" \
+               f"{'+' if complex_add[1] > 0 and complex_add[0] !=0 else ''}" \
+               f"{complex_add[1] if complex_add[1] !=0 else ''}i"
 
     def __mul__(self, other):
         complex_mul = []
@@ -21,21 +22,36 @@ class MyComplex:
                            (int(self.conversion(self.complex_str)[1]) * int(other.conversion(other.complex_str)[1])))
         complex_mul.append((int(self.conversion(self.complex_str)[1]) * int(other.conversion(other.complex_str)[0])) +
                            (int(self.conversion(self.complex_str)[0]) * int(other.conversion(other.complex_str)[1])))
-        return f"{self.complex_str}+{other.complex_str}={complex_mul[0]}{'+' if complex_mul[1] > 0 else ''}" \
-               f"{complex_mul[1]}i"
+        return f"({self.complex_str})*({other.complex_str})={complex_mul[0] if complex_mul[0] !=0 else ''}" \
+               f"{'+' if complex_mul[1] > 0 and complex_mul[0] !=0 else ''}" \
+               f"{complex_mul[1]if complex_mul[1] !=0 else '' }i"
 
     @staticmethod
     def conversion(complex_str):
         complex_list = []
         complex_str = complex_str.replace("i", "")
-        for index in range(1, len(complex_str)):
-            if complex_str[index] == "-" or complex_str[index] == "+":
-                complex_list.append(complex_str[:index])
-                complex_list.append(complex_str[index:])
+        if len(complex_str) == 0:
+            complex_str = "1"
+        if len(complex_str) == 1:
+            complex_list.append(complex_str)
+            complex_list.insert(0, "0")
+        else:
+            for i in range(1, len(complex_str)):
+                if complex_str[i] == "-" or complex_str[i] == "+":
+                    complex_list.append(complex_str[:i])
+                    complex_list.append(complex_str[i:])
+                else:
+                    complex_list.append(complex_str)
+        if len(complex_list) == 1:
+            complex_list.insert(0, "0")
+        if complex_list[-1] == "-" or complex_list[-1] == "+":
+            complex_list[-1] = complex_list[-1] + "1"
+
         return complex_list
 
 
-my_complex1 = MyComplex("-2+3i")
-my_complex2 = MyComplex("-1+3i")
+my_complex1 = MyComplex("3i")
+my_complex2 = MyComplex("i")
 print(my_complex1 + my_complex2)
 print(my_complex1 * my_complex2)
+
